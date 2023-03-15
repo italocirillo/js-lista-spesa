31f75ede7d77fdcdc2fa5036e5475647a2833ccc
italocirillo <italocirillo@gmail.com>
2023-03-15 21:07:02 +0100

    ESERCIZIO COMPLETO

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("javascript collegato");
+// INIZIALIZZAZIONI VARIABILI
+const listaSpesa = document.querySelector(".lista-spesa");
+const elementiListaSpesa = ["pane","latte","uova","mais","acqua","tonno","basilico","prosciutto","torta al cioccolato","patate"];
+
+let elementi="";
+
+i=0;
+while(i < elementiListaSpesa.length){
+    elementi += `<li>${elementiListaSpesa[i]}</li>`;
+    i++;
+}
+listaSpesa.innerHTML = elementi;
